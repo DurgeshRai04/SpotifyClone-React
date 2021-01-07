9712a6c8699c1932e1d0fa753f2daefc1b8cca7a
Durgesh <durgeshrai2012@gmail.com>
2021-01-07 08:44:23 +0530

    Clean Up and Login page added

---
+++ src/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="login">
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
+        alt="spotify-login"
+      ></img>
+      <a>LOGIN WITH SPOTIFY</a>
+    </div>
+  );
+}
+kkkxx
+export default Login;
