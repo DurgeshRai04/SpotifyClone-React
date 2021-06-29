62b1598e1b354f4e712c08ab4468704018cc5483
Denver <70090559+Denver44@users.noreply.github.com>
2021-06-30 01:24:42 +0530

    Delete spotify.js

--- src/spotify.js
+++
@@ -1,6 +0,0 @@
-// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
-
-// This is authorization endPoint where Authorziation will be happen by Spotify
-export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectURL = "http://localhost:3000";
-const clientID = "bd58a9e3d6b4414ea7ec473bbf99f916";
